5a839f024855a554f71eeef7605ac8a845cbdd17
sansayan01 <msayan9733@gmail.com>
2026-06-30 19:01:04 +0530

    chore: release v1.0.1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,31 +5,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:microflow_pro/app.dart';
 import 'package:microflow_pro/core/providers/storage_providers.dart';
 import 'package:microflow_pro/core/constants/enums.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-
-  testWidgets('App builds without crashing', (tester) async {
-    await tester.runAsync(() async {
-      SharedPreferences.setMockInitialValues({});
-      final prefs = await SharedPreferences.getInstance();
-
-      await tester.pumpWidget(
-        ProviderScope(
-          overrides: [
-            sharedPreferencesProvider.overrideWithValue(prefs),
-          ],
-          child: const MicroFlowApp(),
-        ),
-      );
-
-      await tester.pump();
-      expect(find.byType(MaterialApp), findsOneWidget);
-    });
-  });
 
   testWidgets('Theme provider initializes correctly', (tester) async {
     SharedPreferences.setMockInitialValues({});
